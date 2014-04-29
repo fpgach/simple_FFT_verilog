a18b444862ea16278051580066faa82b6accf8be
Evgenii <zubarev.e.v@gmail.com>
2014-04-28 21:57:17 -0700

    Update complex_mult.v
    delete comments

--- src/complex_mult.v
+++ src/complex_mult.v
@@ -11,12 +11,7 @@ module complex_mult(
 wire    signed  [ 27 :  0 ] w_Re = Re_1 * Re_2 - Im_1 * Im_2 + 16'sd512;
 wire    signed  [ 27 :  0 ] w_Im = Re_1 * Im_2 + Re_2 * Im_1 + 16'sd512;
 
-wire    signed  [ 27 :  0 ] w_Re1Im2 = Re_1 * Im_2;
-wire    signed  [ 27 :  0 ] w_Re2Im1 = Re_2 * Im_1;
-
 assign Re_out = w_Re[10+:16];
-//assign Re_out = w_Re[27] ? w_Re[10+:16] + 1'b1 : w_Re[10+:16];
 assign Im_out = w_Im[10+:16];
-//assign Im_out = w_Im[27] ? w_Im[10+:16] + 1'b1 : w_Im[10+:16];
 
 endmodule
