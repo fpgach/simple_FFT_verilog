5a91dd07bdd18858ca0ea9c59c07695bd93386c1
Evgenii <zubarev.e.v@gmail.com>
2014-04-28 21:58:21 -0700

    Update BF4_comb.v
    delete comments

--- src/BF4_comb.v
+++ src/BF4_comb.v
@@ -19,48 +19,23 @@ module BF4_comb(
 
     
 wire    signed  [ 15 :  0 ] w_reout_0 = re_0 + re_2 + re_1 + re_3;
-
-//assign reout_0 = w_reout_0 >>> 2;
 assign reout_0 = w_reout_0[15]  ? (w_reout_0 + 8'sd2) >>> 2 
                                 : w_reout_0 >>> 2;
-
 assign imout_0 = 16'd0;
-
-
-
 
 wire    signed  [ 15 :  0 ] w_reout_1 = re_0 - re_2;
 wire    signed  [ 15 :  0 ] w_imout_1 = re_3 - re_1;
-
-//assign reout_1 = w_reout_1 >>> 1;
 assign reout_1 = w_reout_1[15] ? (w_reout_1 + 8'sd1) >>> 1 : w_reout_1 >>> 1;
-//assign imout_1 = w_imout_1 >>> 1;
 assign imout_1 = w_imout_1[15] ? (w_imout_1 + 8'sd1) >>> 1 : w_imout_1 >>> 1;
 
-
-
 wire    signed  [ 15 :  0 ] w_reout_2 = re_0 + re_2 - re_1 - re_3;
-
-//assign reout_2 = w_reout_2 >>> 2;
 assign reout_2 = w_reout_2[15]  ? (w_reout_2 + 8'sd2) >>> 2 
                                 : w_reout_2 >>> 2;
-
 assign imout_2 = 16'd0;
-
-
-
-
-
 
 wire    signed  [ 15 :  0 ] w_reout_3 = re_0 - re_2;
 wire    signed  [ 15 :  0 ] w_imout_3 = re_1 - re_3;
-
-//assign reout_3 = w_reout_3 >>> 1;
 assign reout_3 = w_reout_3[15] ? (w_reout_3 + 8'sd1) >>> 1 : w_reout_3 >>> 1;
-
-//assign imout_3 = w_imout_3 >>> 1;
 assign imout_3 = w_imout_3[15] ? (w_imout_3 + 8'sd1) >>> 1 : w_imout_3 >>> 1;
-
-
 
 endmodule
